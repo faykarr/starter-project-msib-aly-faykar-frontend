81ae17c4b2f82a199e8d124bcb912ff73d0fadad
faykarr <nasyathfaykar@gmail.com>
2024-04-19 19:59:27 +0700

    feat: FINISHED ALL THE FEATURES

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ This is just a simple sign in and sign up page for UPTKOMP. This project is made
 
 ## Check up the design
 [Design](https://www.figma.com/file/dkiLNNYdlrNabJRlBwn1gK/starter-project-msib-aly-faykar-frontend?type=design&node-id=0%3A1&mode=design&t=N9AmYhsJW1ghmWTR-1)
+
+## Disclaimer
+This project is not responsive for now. It's just a simple sign in and sign up page & profile page. It's not recommended to open this project on mobile devices.
+
+## Some Screenshots
+![Sign In](src\assets\img\login.jpeg)
+![Sign Up](src\assets\img\register.jpeg)
+![Profile](src\assets\img\profile.jpeg)
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 function Navbar() {
   return (
     <div className="flex justify-around items-center w-full py-6">
-      <button className="w-full pl-10">
-        <img src="/src/assets/icons/menu.svg" alt="Menu Icons" />
-      </button>
-      <div className="flex flex-row gap-8 text-textSecondary pr-20 cursor-pointer">
+      <div className="w-full pl-10">
+        <img
+          src="/src/assets/icons/menu.svg"
+          alt="Menu Icons"
+          className="cursor-pointer"
+        />
+      </div>
+      <div className="flex flex-row gap-8 text-textSecondary pr-20">
         <div className="flex flex-col justify-center items-start">
           <h1 className="font-poppins font-medium text-xl">Faykarr</h1>
           <p className="font-light">Administrator</p>
@@ -12,7 +16,7 @@ function Navbar() {
         <img
           src="/src/assets/img/avatar.jpg"
           alt="Avatar"
-          className="rounded-full w-16 h-16"
+          className="rounded-full w-16 h-16 cursor-pointer"
         />
       </div>
     </div>
@@ -22,13 +26,13 @@ function Navbar() {
 function Content() {
   return (
     <div className="grid grid-cols-3 justify-center items-center px-20 py-7 text-white gap-11">
-      <div className="w-full flex flex-col items-center justify-between py-5 rounded-md bg-primaryMain border-[3px] border-primaryBorder shadow-card">
+      <div className="w-full flex flex-col items-center justify-around py-10 rounded-md bg-primaryMain border-[3px] border-primaryBorder shadow-card h-full">
         <img
           src="/src/assets/img/avatar.jpg"
           alt="Avatar"
           className="rounded-full w-48"
         />
-        <div className="flex flex-col justify-center items-center py-5 text-textSecondary">
+        <div className="flex flex-col justify-center items-center text-textSecondary">
           <h3 className="font-medium text-xl">Nasyath Faykar</h3>
           <p className="font-normal text-base">Senior Asisten UPTKOMP</p>
         </div>
@@ -37,7 +41,61 @@ function Content() {
           <p className="text-white font-medium text-base">Change Avatar</p>
         </button>
       </div>
-      <div className="col-span-2">Testing 2</div>
+      <div className="col-span-2 bg-primaryMain border-[3px] border-primaryBorder h-full rounded-md shadow-card px-7 py-5">
+        <div className="flex flex-col items-start justify-center gap-2">
+          <p className="text-base text-textSecondary font-normal">
+            Nomor Induk Mahasiswa
+          </p>
+          <input
+            type="text"
+            className="pl-5 pr-3 py-2 bg-primaryBorder border border-primaryBorder rounded-md focus:outline-none focus:border-secondaryMain transition-all w-full text-textSecondary"
+            value="21.230.0194"
+            disabled="true"
+          />
+        </div>
+        <div className="flex flex-col items-start justify-center gap-2 mt-5">
+          <p className="text-base text-textSecondary font-normal">
+            Nama Lengkap
+          </p>
+          <input
+            type="text"
+            className="pl-5 pr-3 py-2 bg-primarySurface border border-primaryBorder rounded-md focus:outline-none focus:border-secondaryMain transition-all w-full text-textSecondary"
+            value="Nasyath Faykar"
+          />
+        </div>
+        <div className="flex flex-col items-start justify-center gap-2 mt-5">
+          <p className="text-base text-textSecondary font-normal">Username</p>
+          <input
+            type="text"
+            className="pl-5 pr-3 py-2 bg-primaryBorder border border-primaryBorder rounded-md focus:outline-none focus:border-secondaryMain transition-all w-full text-textSecondary"
+            value="faykarr"
+            disabled="true"
+          />
+        </div>
+        <div className="flex flex-col items-start justify-center gap-2 mt-5">
+          <p className="text-base text-textSecondary font-normal">Email</p>
+          <input
+            type="text"
+            className="pl-5 pr-3 py-2 bg-primarySurface border border-primaryBorder rounded-md focus:outline-none focus:border-secondaryMain transition-all w-full text-textSecondary"
+            value="teknisi.faykarr@stmik-wp.ac.id"
+          />
+        </div>
+        <button className="flex flex-row gap-3 justify-around items-center px-12 py-2 bg-secondaryMain text-white rounded-lg font-medium text-lg mt-5">
+          <p className="text-white font-medium text-base">Save Changes</p>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="flex justify-between items-center w-full bg-primaryMain text-textSecondary px-10 py-3">
+      <p className="text-base font-medium">2024 © Faykarr</p>
+      <p className="text-base font-medium">
+        Crafted with ❤️ by{" "}
+        <span className="text-secondaryHover cursor-pointer">faykarr</span>
+      </p>
     </div>
   );
 }
@@ -53,6 +111,7 @@ function Profile() {
         </p>
       </div>
       <Content />
+      <Footer />
     </>
   );
 }
